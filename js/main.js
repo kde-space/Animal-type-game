fb3dfd8ad8f91a4baf7ceda3d4b472b5a6e9092d
koji.kobayashi <kojikobayashi1840@gmail.com>
2022-08-31 01:31:55 +0900

    test createjs

--- js/main.js
+++ js/main.js
@@ -1,27 +1,76 @@
 'use strict';
 
+document.addEventListener('DOMContentLoaded', init1);
+
+function init1() {
+  const canvas1 = document.getElementById('canvas1');
+  canvas1.width = 800;
+  canvas1.height = 600;
+
+  const stage = new createjs.Stage('canvas1');
+  const object = new createjs.Shape();
+  object.graphics.beginFill('DarkRed');
+  object.graphics.beginStroke('Blue');
+  object.graphics.setStrokeStyle(10);
+  object.graphics.drawCircle(100, 100, 100);
+  // object.x = 100;
+  // object.y = 100;
+  // object.scaleY = 0.5;
+
+  var poly = new createjs.Shape();
+  poly.graphics.beginFill("DarkRed"); // 赤色で描画するように設定
+  poly.graphics.drawPolyStar(300, 300, 75, 10, 0.3, -90);
+
+  var obj = new createjs.Shape();
+  obj.graphics.beginFill("Green"); // 赤色で描画するように設定
+  obj.graphics.moveTo(200, 500); // (0,0)座標から描き始める
+  obj.graphics.lineTo(100, 0); // (100,0)座標まで辺を描く
+  obj.graphics.lineTo(0, 100); // (0,100)座標まで辺を描く
+  obj.graphics.lineTo(0, 0); // (0,0)座標まで辺を描く
+  stage.addChild(obj); // 表示リストに追加
+
+  stage.addChild(object);
+  stage.addChild(poly);
+  stage.update();
+}
+
+
+
+
 const dpr = window.devicePixelRatio;
 const CANVAS_SIZE = {
-  width: 1280,
-  height: 720
+  width: 600,//window.innerWidth,
+  height: 400//window.innerHeight
 }
 const IMG_URL_LIST = [
   'img/dog_akitainu.png',
   'img/dog_doberman.png',
-  'img/dog_rottweiler.png'
+  'img/dog_rottweiler.png',
+  'img/dog_kooikerhondje.png',
+  'img/shoes_32.png',
+  'img/animal_bear_higuma.png',
+  'img/animal_bear_panda.png',
+  'img/animal_kirin.png',
+  'img/animal_lion.png',
+  'img/animal_panda_back.png',
+  'img/animal_penguin.png',
+  'img/animal_uma.png',
+  'img/animal_usagi_gray.png',
+  'img/animal_zou.png',
 ];
 
 const imgsOnCanvas = [];
 
 class Animal {
-  constructor(img, x, y, scale) {
+  constructor(img, x, y, scale, velocity) {
     this.id = null;
     this.img = img;
     this.x = x;
     this.y = y;
     this.scale = scale;
-    this.velocity = 1;
+    this.velocity = velocity;
     this.isOver = false;
+
   }
 
   /**
@@ -33,7 +82,6 @@ class Animal {
   }
 
   update() {
-    console.log(`${this.name}: `, this.y);
     if(this.y < -this.img.height * this.scale) {
       console.log('上限超えた！');
       this.isOver = true;
@@ -47,10 +95,11 @@ class Animal {
   }
 }
 
-document.addEventListener('DOMContentLoaded', init);
+// document.addEventListener('DOMContentLoaded', init);
 
 async function init() {
   const canvas = document.getElementById('canvas');
+  if (!canvas) return;
   canvas.width = CANVAS_SIZE.width;
   canvas.height = CANVAS_SIZE.height;
   canvas.style.width = CANVAS_SIZE.width;
@@ -70,8 +119,38 @@ async function init() {
   }
 }
 
+/**
+ * 
+ * @param {Number} min 
+ * @param {Number} max 
+ * @param {Integer} n 
+ * @returns {Number}
+ */
+function random(min, max, decimalPlaces = 0) {
+  if (min >= max) {
+    throw new Error('"max" must be greater than "min"');
+  }
+  const decimalPlacesNum = 10 ** decimalPlaces;
+  const newMin = min * decimalPlacesNum;
+  const newMax = max * decimalPlacesNum;
+  const result = Math.floor(Math.random() * (newMax + 1 - newMin) + newMin);
+  return result / decimalPlacesNum;
+}
+
 function exe(images, ctx) {
-  imgsOnCanvas.push(new Animal(images[0], 50, 300, 0.5));
+  const pickedImage = images[random(0, images.length - 1)];
+  const scale = random(0.3, 0.8, 1);
+  console.log('scale :>> ', scale);
+  const velocityBase = 3;
+  imgsOnCanvas.push(
+    new Animal(
+      pickedImage, 
+      Math.floor(Math.random() * (CANVAS_SIZE.width - pickedImage.width * scale)),
+      CANVAS_SIZE.height, 
+      scale,
+      velocityBase / (scale)
+    )
+  );
   // requestAnimationFrame が二重に実行されるのを防止
   if (imgsOnCanvas.length > 1) {
     return;
@@ -92,33 +171,14 @@ function loop(ctx) {
     animal.update();
     animal.draw(ctx);
   });
+
   const id = window.requestAnimationFrame(() => {
     loop(ctx);
   });
+  
   if (imgsOnCanvas.length === 0) {
     cancelAnimationFrame(id);
-    // isNoImgOnCanvas = true;
   }
-}
-
-/**
- * 画像を配置
- * @param {HTMLImageElement} img 
- * @param {CanvasRenderingContext2D} ctx 
- * @param {Number} x 
- * @param {Number} y 
- * @param {Number} scale 
- */
-function addImage(img, ctx, x, y, scale) {
-  let cnt = 0;
-  const draw = () => {
-    ctx.clearRect(0, 0, CANVAS_SIZE.width, CANVAS_SIZE.height); // clear the canvas
-    ctx.drawImage(img, x, y + -cnt, img.width * scale, img.height * scale);
-    cnt++;
-    console.log('cnt :>> ', cnt);
-    window.requestAnimationFrame(draw);
-  }
-  window.requestAnimationFrame(draw);
 }
 
 /**
